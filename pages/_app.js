176093677c6eaa44dc4d0ec4aabe083653b5a56a
nickfff-dev <hunchographics@gmail.com>
2024-05-24 20:27:24 +0300

    ui prep fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { ThemeProvider } from "@material-tailwind/react";
+import { Ubuntu_Mono} from 'next/font/google'
 
+const mono = Ubuntu_Mono({subsets: ['latin'], weight: ['400', '700']})
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <main className={mono.className}>
+  <Component {...pageProps} />
+</main>
+);
 }
